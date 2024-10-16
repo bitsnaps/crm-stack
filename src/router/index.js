0c13e4865680229f2d2d510f6786c27070540b28
bhalouane <bhalouane@yahoo.fr>
2024-10-16 18:44:45 +0000

    fix: components path

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Dashboard from './components/Dashboard.vue'
-import Contacts from './components/Contacts.vue'
-import Opportunities from './components/Opportunities.vue'
+import Dashboard from '../components/Dashboard.vue'
+import Contacts from '../components/Contacts.vue'
+import Opportunities from '../components/Opportunities.vue'
 
 const router = createRouter({
   history: createWebHistory(),
